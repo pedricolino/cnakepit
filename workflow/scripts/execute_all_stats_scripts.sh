91632aec849fed8b9da3b4050208c621164b34c7
Cedric Moris <cedric.moris@bih-charite.de>
2024-02-07 15:42:44 +0100

    add wrapper script to get multiple types of result stats and adjust other scripts accordingly

---
+++ workflow/scripts/execute_all_stats_scripts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p results/stats
+
+echo "\nCompute mean gene coverage for all samples with CNVkit's CNN output files\n"
+Rscript workflow/scripts/calc_mean_gene_coverage.R
+
+echo "\nCollect all benchmarks into one file\n"
+python workflow/scripts/collect_benchmarks.py
+
+echo "\nCount the total number of variants and the number of variants with PASS in the filter column\n"
+./workflow/scripts/count_filtered_vcf_variants.sh
+
+echo "\nCount SNVs found by Mutect2\n"
+./workflow/scripts/count_snv.sh
+
+echo "\nCollect all PureCN optima for tumor purity and ploidy information for all samples\n"
+Rscript workflow/scripts/collect_purecn_optima.R $(ls -d results/purecn/*/ | xargs)
