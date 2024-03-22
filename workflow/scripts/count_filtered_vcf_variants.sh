bfc0ae934b99ef9ba1c88554ddbc43b53e2da346
Cedric Moris <cedric.moris@bih-charite.de>
2024-03-22 20:14:52 +0100

    change script file permissions

--- workflow/scripts/count_filtered_vcf_variants.sh
+++ workflow/scripts/count_filtered_vcf_variants.sh
@@ -3,7 +3,7 @@
 output_file="results/stats/mutect2_vcf_filtered_variants_counts.tsv"
 
 # Create the TSV file and add the header
-echo -e "SampleName\tTotalVariants\tPassedVariants" > "$output_file"
+echo -e "SampleName\tTotalVariants\tPassedVariants\tGermlineVariants" > "$output_file"
 
 # Get the total number of files
 total_files=$(ls results/mutect2/filtered/*.vcf.gz | wc -l)
@@ -16,10 +16,13 @@ for file in results/mutect2/filtered/*.vcf.gz; do
     # Count the number of variants with PASS in the filter column
     pass_variants=$(zcat "$file" | grep '^[^#]*PASS' | wc -l)
 
+    # Count the number of variants with germline in the filter column
+    germline_variants=$(zcat "$file" | grep '^[^#]*germline' | wc -l)
+
     SampleName=$(basename "$file" | sed -e 's/_filtered.vcf.gz//g')
 
     # Append the counts to the TSV file
-    echo -e "$SampleName\t$total_variants\t$pass_variants" >> "$output_file"
+    echo -e "$SampleName\t$total_variants\t$pass_variants\t$germline_variants" >> "$output_file"
 
     # Update the progress
     processed_files=$((processed_files + 1))
--- workflow/scripts/execute_all_stats_scripts.sh
+++ workflow/scripts/execute_all_stats_scripts.sh
@@ -2,6 +2,15 @@
 
 mkdir -p results/stats
 
+printf "\nCollect subset of specific (e.g. WES-matching) samples in one folder (results/collection_of_specific_samples).\n"
+./workflow/scripts/collect_purecn_results_for_specific_samples.sh ~/work/sign-oc/wes_panel_results/corresponding_panels_names.tsv
+
+printf "\nCount the number of successful PureCN runs\n"
+./workflow/scripts/count_successful_purecn_runs.sh
+
+printf "\nCollect for all samples the percentage of reads mapped to the panel design\n"
+./workflow/scripts/collect_perc_reads_mapped_to_paneldesign.sh
+
 printf "\nCompute mean gene coverage for all samples with CNVkit's CNN output files\n"
 Rscript workflow/scripts/calc_mean_gene_coverage.R
 
@@ -19,3 +28,4 @@ Rscript workflow/scripts/collect_purecn_optima.R $(ls -d results/purecn/*/ | xar
 
 printf "\nCollect for all genes and samples the absolute copy numbers and log2 ratios found by PureCN\n"
 Rscript workflow/scripts/collect_gene_copy_numbers_log2ratios.R $(ls -d results/purecn/*/ | xargs)
+
