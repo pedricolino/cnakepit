9eae6e212a5109c0b6a416a5e7b46624d50bd706
Cedric Moris <cedric.moris@bih-charite.de>
2024-04-04 17:44:00 +0200

    replace fixed name by finding the right file

--- workflow/scripts/collect_purecn_results_for_specific_samples.sh
+++ workflow/scripts/collect_purecn_results_for_specific_samples.sh
@@ -10,9 +10,9 @@ for pon in results/purecn*; do
     for branch in $(ls -d $pon/*/); do
         mkdir -p $OUT/$branch
         while IFS= read -r name; do
-            cp "${branch}${name}/${name}_dnacopy.seg" "results/collection_of_specific_samples/${branch}${name}_dnacopy.seg"
-            cp "${branch}${name}/${name}_genes.csv" "results/collection_of_specific_samples/${branch}${name}_genes.csv"
-            cp "${branch}${name}/${name}.rds" "results/collection_of_specific_samples/${branch}${name}.rds"
-        done < $sample_names_table
+            cp $(find $branch -name "$name*_dnacopy.seg") "results/collection_of_specific_samples/${branch}${name}_dnacopy.seg"
+            cp $(find $branch -name "$name*_genes.csv") "results/collection_of_specific_samples/${branch}${name}_genes.csv"
+            cp $(find $branch -name "$name*.rds") "results/collection_of_specific_samples/${branch}${name}.rds"
+        done <$sample_names_table
     done
 done
