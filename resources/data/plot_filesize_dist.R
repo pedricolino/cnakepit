8965e15e7e0808571e47a349ebeafa7a83f799a4
Cedric Moris <cedric.moris@bih-charite.de>
2024-04-04 17:47:22 +0200

    fix plot x scale and switch to ggplot

--- resources/data/plot_filesize_dist.R
+++ resources/data/plot_filesize_dist.R
@@ -5,17 +5,28 @@ input_file <- if (length(commandArgs(trailingOnly = TRUE)) > 0) {
     "all_samples_filepath_size.tsv"
 }
 
-df <- read.table(input_file, sep="\t", header=TRUE)
+library(ggplot2)
+df <- read.table(input_file, sep = "\t", header = TRUE)
 
 # Save plot as PNG
-png(filename="file_size_distribution.png",
-    width = 480, height = 770)
+png(
+    filename = "file_size_distribution.png",
+    width = 480, height = 770
+)
 
-# Plot file size in log10 scale
-hist(log10(df$FileSize), xlab="File size", main="File Size Distribution", breaks=50, xaxt='n', col="lightblue")
-# Set x-axis labels to file size units
-axis(side = 1, at = 1:11, labels=c("10 B","100 B","1 KB","10 KB","100 KB","1 MB","10 MB","100 MB","1 GB","10 GB", "100 GB"))
+cutoff_filesize <- 1e8
 
-# Save the plot
+ggplot(df, aes(x = log10(FileSize))) +
+    geom_histogram(bins = 100, fill = "skyblue", color = "black") +
+    labs(
+        x = "File size)",
+        y = "Number of samples",
+    ) +
+    scale_x_continuous(
+        breaks = 1:11,
+        labels = c("10 B", "100 B", "1 KB", "10 KB", "100 KB", "1 MB", "10 MB", "100 MB", "1 GB", "10 GB", "100 GB"),
+        limits = c(1, 11) # Add this line
+    ) +
+    geom_vline(xintercept = log10(cutoff_filesize), linetype = "dashed", color = "red")
+
 dev.off()
-
