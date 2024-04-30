4fb18a7ab1647b65b96fdcccee5eb1716133005f
pedricolino <127396917+pedricolino@users.noreply.github.com>
2024-04-30 11:01:48 +0200

    Update paths in count_successful_purecn_runs.sh

--- workflow/scripts/count_successful_purecn_runs.sh
+++ workflow/scripts/count_successful_purecn_runs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-find results/ -name '*rds*' | grep -o "results/[a-zA-Z_]*/[a-zA-Z_]*" | uniq -c >results/stats/successful_purecn_runs.txt
+find results/ -name '*rds*' | grep -o "results/[a-zA-Z_]*/[a-zA-Z_]*/*[^0-9]*/" | uniq -c >results/stats/successful_purecn_runs.txt
+#find results/ -name '*rds*' | grep -o "results/[a-zA-Z_]*/[a-zA-Z_]*" | uniq -c >results/stats/successful_purecn_runs.txt
