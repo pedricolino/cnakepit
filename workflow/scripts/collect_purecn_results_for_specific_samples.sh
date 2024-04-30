1a8d1cee397e5d2e0481360747bbcb0ad495d6bc
pedricolino <127396917+pedricolino@users.noreply.github.com>
2024-04-30 11:00:54 +0200

    correct paths in collect_purecn_results_... script

--- workflow/scripts/collect_purecn_results_for_specific_samples.sh
+++ workflow/scripts/collect_purecn_results_for_specific_samples.sh
@@ -7,23 +7,25 @@ mkdir -p $OUT
 # for every folder starting with results/purecn
 for pon in results/purecn*; do
     echo "Processing $pon"
+    pon_name=$(basename $pon)
+    mkdir -p $OUT/$pon_name
     for branch in $(ls -d $pon/*/); do
         branch_name=$(basename $branch)
-        mkdir -p $OUT/$branch_name
+        mkdir -p $OUT/$pon_name/$branch_name
         while IFS= read -r name; do
             seg_files=$(find $branch -name "$name*_dnacopy.seg")
             if [ -n "$seg_files" ]; then
-                cp "$seg_files" "$OUT/$branch_name/${name}_dnacopy.seg"
+                cp "$seg_files" "$OUT/$pon_name/$branch_name/${name}_dnacopy.seg"
             fi
 
             genes_files=$(find $branch -name "$name*_genes.csv")
             if [ -n "$genes_files" ]; then
-                cp "$genes_files" "$OUT/$branch_name/${name}_genes.csv"
+                cp "$genes_files" "$OUT/$pon_name/$branch_name/${name}_genes.csv"
             fi
 
             rds_files=$(find $branch -name "$name*.rds")
             if [ -n "$rds_files" ]; then
-                cp "$rds_files" "$OUT/$branch_name/${name}.rds"
+                cp "$rds_files" "$OUT/$pon_name/$branch_name/${name}.rds"
             fi
         done <$sample_names_table
     done
